c17f1163a46f02944d75954343e93b9a0cb7850e
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2020-07-19 16:48:04 +0300

    Add models

---
+++ MiddleEducationPlan/MiddleEducationPlan/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddleEducationPlan.Models
+{
+    public class Project : TableEntity
+    {
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string EntityName { get; set; }
+        public IEnumerable<Task> Tasks { get; set; }
+    }
+}
