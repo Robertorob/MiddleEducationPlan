31b9c7fb85fde3dc1be260ed23a6efe09edc378e
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2020-08-15 17:33:35 +0300

    Add first unit test

---
+++ MiddleEducationPlan/MiddleEducationPlan.UnitTests/UnitTest1.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MiddleEducationPlan.BusinessLogic.Interfaces;
+using MiddleEducationPlan.BusinessLogic.Services;
+using MiddleEducationPlan.Common.Services;
+using MiddleEducationPlan.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MiddleEducationPlan.UnitTests
+{
+    public class Tests
+    {
+        private ProjectController projectController;
+        private Guid projectEntityId;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.projectEntityId = Guid.NewGuid();
+            var projectEntity = new BusinessLogic.TableEntities.ProjectEntity()
+            {
+                Id = this.projectEntityId
+            };
+
+            var projectServiceMock = new Mock<IProjectService>();
+            projectServiceMock.Setup(f => f.GetProjectByIdAsync(this.projectEntityId)).Returns(Task.FromResult(projectEntity));
+
+            this.projectController = new ProjectController(projectServiceMock.Object);
+        }
+
+        [Test]
+        public async Task Get_ExistingProject_Ok200()
+        {
+            var result = await this.projectController.Get(this.projectEntityId) as ObjectResult;
+
+            Assert.AreEqual(result.StatusCode, (int) HttpStatusCode.OK);
+        }
+    }
+}
