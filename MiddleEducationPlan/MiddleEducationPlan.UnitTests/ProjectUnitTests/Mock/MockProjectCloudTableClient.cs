f2c720dc26ccd33b8165f12c9b548334acc7c835
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2020-09-06 17:41:59 +0300

    Remove unnecessary usings

--- MiddleEducationPlan/MiddleEducationPlan.UnitTests/ProjectUnitTests/Mock/MockProjectCloudTableClient.cs
+++ MiddleEducationPlan/MiddleEducationPlan.UnitTests/ProjectUnitTests/Mock/MockProjectCloudTableClient.cs
@@ -1,13 +1,7 @@
 ﻿
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Table;
-using MiddleEducationPlan.BusinessLogic.TableEntities;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiddleEducationPlan.UnitTests.Project.Mock
 {
