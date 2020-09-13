c83c2748413be16ec7b0a3115c9a46655d3b2703
Роберт Ахметшин <RDAhmetshin@stud.kpfu.ru>
2020-09-13 22:47:21 +0300

    Extract ProjectView.js and add jquery-ui

---
+++ MiddleEducationPlan/MiddleEducationPlan/wwwroot/ProjectView.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    console.log("ready!");
+});
+
+function CreateProject() {
+    let name = $("#projectName").val();
+
+    let project = {};
+    project.Name = name;
+
+    $.post("CreatePost", project, (result) => {
+        if (result.status === 0) {
+            $("#dialog").dialog();
+            //window.alert("success");
+            ClearForm();
+        }
+        else {
+            window.alert(result.errorMessage);
+        }
+    })
+}
+
+function ClearForm() {
+    $("#projectName").val("");
+}
